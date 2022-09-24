42920cc05c03e81bd6e42f7a74fe8e13681a3d46
t4 43r <troltoh@gmail.com>
2022-09-24 10:31:28 +0800

    Добавьте файлы проекта.

---
+++ ConsoleApp1-3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApplication
+{
+    public class Logic
+    {
+        public static string Rearrangement(string word, string outPut)
+        {
+            if (word.Length % 2 == 0)
+            {
+                for (int i = 0; i < word.Length - 1; i += 2)
+                {
+                    outPut = outPut + word[i + 1] + word[i];
+
+                }
+               
+            }
+            else { outPut = "Введено неверное слово, перезапустите программу и повторите попытку"; }
+
+            return outPut;
+        }
+    }
+     internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово-");
+            string word = Console.ReadLine();
+
+            string outPut = "";
+            var outPutMessage = Logic.Rearrangement( word, outPut);
+            Console.WriteLine(outPutMessage);
+
+        }
+    }
+
+}
+
---
+++ ConsoleApp1-3Tests/LogicTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirstApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApplication.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void RearrangementTest1()
+        {
+            string word = "abcd";
+            var outPut = "";
+            var message = Logic.Rearrangement(word, outPut);
+            Assert.AreEqual("badc", message);
+        }
+        [TestMethod()]
+        public void RearrangementTest2()
+        {
+            string word = "abcde";
+            var outPut = "";
+            var message = Logic.Rearrangement( word, outPut);
+            Assert.AreSame("Введено неверное слово, перезапустите программу и повторите попытку", message);
+        }
+    }
+}
